7504ec4b3e5827163ed76a781e2487caf08c7eff
Robert Miklas <robert.miklas@protonmail.com>
2024-09-27 12:39:28 +0200

    argocdautopilot (#2)
    * added dockerfile for kubectl

--- github_puller/github_puller.sh
+++ github_puller/github_puller.sh
@@ -26,7 +26,7 @@ PULL_ATTEMPTS=${PULL_ATTEMPTS:-3}
 
 # Function to clone the repository if not already present
 clone_repository() {
-  if [ ! -d "$GITPULLER_REPO_PATH/.git" ]; then
+    if [ ! -d "$GITPULLER_REPO_PATH/.git" ]; then
     echo "Directory $GITPULLER_REPO_PATH does not exist or is not a Git repository. Cloning the repository..."
     git clone "$GITPULLER_REPO_URL" "$GITPULLER_REPO_PATH"
     

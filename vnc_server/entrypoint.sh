3322c9eff1c7627e0d707f9f43cb69b0bb5940d5
rmikl <robert.miklas@protonmail.com>
2024-10-28 09:50:32 +0100

    fixes in vnc

--- vnc_server/entrypoint.sh
+++ vnc_server/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 # Set default password if not provided
 VNC_PASSWORD=${VNC_PASSWORD:-password}
 
@@ -8,5 +11,9 @@ mkdir -p /root/.vnc
 echo "$VNC_PASSWORD" | vncpasswd -f > /root/.vnc/passwd
 chmod 600 /root/.vnc/passwd
 
-# Execute the command
+# Ensure .Xauthority exists
+touch /root/.Xauthority
+chmod 600 /root/.Xauthority
+
+# Start the VNC server
 exec "$@"
